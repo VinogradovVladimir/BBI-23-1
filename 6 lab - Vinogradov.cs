3ee8a561ef9d3b165bd1ee6bf226be152ce4b11c
VinogradovVladimir <146069649+VinogradovVladimir@users.noreply.github.com>
2024-02-22 11:35:47 +0300

    Add files via upload

--- 6 lab - Vinogradov.cs
+++ 6 lab - Vinogradov.cs
@@ -1,9 +1,12 @@
 ﻿using System;
 using System.Diagnostics;
+using System.Diagnostics.CodeAnalysis;
 using System.Diagnostics.Metrics;
 using System.Diagnostics.Tracing;
 using System.Net.Sockets;
 using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Security.Cryptography.X509Certificates;
 using System.Xml.Linq;
 using static System.Net.Mime.MediaTypeNames;
 #region 1_1
@@ -27,34 +30,25 @@ struct Results
         Console.Write("{0, -20}", $"{result1}");
         Console.Write("{0, -20}", $"{result2}");
     }
-    public double open_result1 => result1;
-    public double open_result2 => result2;
+    public double open_result => (result1 + result2);
 }
 public class Program
 {
-    public static void Main(string[] args)
+    private Results[] Sort(Results[] table)
     {
-        Results[] table = new Results[5]
-        {
- new Results("первый", "мисис топ", 2.10, 2.20),
- new Results("второй", "мисис топ 2", 2.40, 2.90),
- new Results("третий", "мисис топ 3", 4.50, 3.00),
- new Results("четвёртый", "мисис топ 4", 2.60, 4.00),
- new Results("пятый", "мисис топ 5", 2.70, 5.00)
-        };
         double max;
         int index;
         Results switcher;
         for (int i = 0; i < table.Length - 1; i++)
         {
             bool flag = false;
-            max = (table[i].open_result1 + table[i].open_result2);
+            max = (table[i].open_result);
             index = i;
             for (int j = i + 1; j < table.Length; j++)
             {
-                if ((table[j].open_result1 + table[j].open_result2) > max)
+                if ((table[j].open_result) > max)
                 {
-                    max = (table[j].open_result1 + table[j].open_result2);
+                    max = (table[j].open_result);
                     index = j;
                     flag = true;
                 }
@@ -67,6 +61,20 @@ public class Program
                 flag = false;
             }
         }
+        return table;
+    } 
+    public static void Main(string[] args)
+    {
+        Results[] table = new Results[5]
+        {
+ new Results("первый", "мисис топ", 2.10, 2.20),
+ new Results("второй", "мисис топ 2", 2.40, 1.20),
+ new Results("третий", "мисис топ 3", 4.50, 3.00),
+ new Results("четвёртый", "мисис топ 4", 2.60, 9.00),
+ new Results("пятый", "мисис топ 5", 2.70, 5.00)
+        };
+        var sort = new Program();
+        table=sort.Sort(table);
         Console.WriteLine("Фамилия:\tОбщество:\tРезультат 1 попытки:\tРезультат 2 попытки: ");
         for (int i = 0; i < table.Length; i++)
         {
@@ -79,63 +87,44 @@ public class Program
 /*struct Results
 {
     private string name;
-    private double mark1;
-    private double mark2;
-    private double mark3;
-    private double mark4;
-    private double mark5;
-    public Results(string n, double m1, double m2, double m3, double m4, double m5)
+    private double[] mark;
+    public Results(string n, double [] m)
     {
         name = n;
-        mark1 = m1;
-        mark2 = m2;
-        mark3 = m3;
-        mark4 = m4;
-        mark5 = m5;
+        mark = m;
     }
     public void display()
     {
         Console.Write("\n{0, -18}", $"{name}");
-        Console.Write("{0, -17}", $"{mark1}");
-        Console.Write("{0, -17}", $"{mark2}");
-        Console.Write("{0, -17}", $"{mark3}");
-        Console.Write("{0, -17}", $"{mark4}");
-        Console.Write("{0, -17}", $"{mark5}");
+
+        for (int i=0; i<mark.Length;i++) Console.Write("{0, -17}", $"{mark[i]}");
     }
-    public double open_mark1 => mark1;
-    public double open_mark2 => mark2;
-    public double open_mark3 => mark3;
-    public double open_mark4 => mark4;
-    public double open_mark5 => mark5;
+    public double[] open_mark => mark;
+    public double Sum(double[] m)
+    {
+        double sum = 0;
+        for (int i = 0; i < m.Length; i++) sum += m[i];
+        return sum;
+    }
 }
 public class Program
 {
-    public static void Main(string[] args)
+    private Results[] Sort(Results[] table)
     {
-        Results[] table = new Results[5]
-        {
- new Results("первый", 1, 2,3,4,5),
- new Results("второй", 1, 2,3,4,5),
- new Results("третий", 1, 2,3,4,5),
- new Results("четвёртый",1, 2,3,4,5),
- new Results("пятый",1, 2,3,4,5)
-        };
         double max;
         int index;
         Results switcher;
         for (int i = 0; i < table.Length - 1; i++)
         {
             bool flag = false;
-            max = (table[i].open_mark1 + table[i].open_mark2 + table[i].open_mark3 + table[i].open_mark4 +
-           table[i].open_mark5);
+            var m = new Results();
+            max = m.Sum(table[i].open_mark);
             index = i;
             for (int j = i + 1; j < table.Length; j++)
             {
-                if ((table[j].open_mark1 + table[j].open_mark2 + table[j].open_mark3 + table[j].open_mark4 +
-               table[j].open_mark5) > max)
+                if (m.Sum(table[j].open_mark) > max)
                 {
-                    max = (table[j].open_mark1 + table[j].open_mark2 + table[j].open_mark3 + table[j].open_mark4 +
-                   table[j].open_mark5);
+                    max = m.Sum(table[j].open_mark);
                     index = j;
                     flag = true;
                 }
@@ -148,6 +137,20 @@ public class Program
                 flag = false;
             }
         }
+        return table;
+    }
+    public static void Main(string[] args)
+    {
+        Results[] table = new Results[5]
+        {
+         new Results("первый", [1, 2,3,4,1]),
+         new Results("второй", [1, 2,3,4,2]),
+         new Results("третий", [1, 2,3,4,3]),
+         new Results("четвёртый",[1, 2,3,4,499]),
+         new Results("пятый",[1, 2,3,4,5])
+        };
+        var z = new Program();
+        z.Sort(table);
         Console.WriteLine("Фамилия:\tОценка№1:\tОценка№2\tОценка№3\tОценка№4\tОценка№5");
         for (int i = 0; i < table.Length; i++)
         {
@@ -192,6 +195,36 @@ struct Static
 }
 public class Program
 {
+    private Static[] Sort(Static[] gar)
+    {
+        double max;
+        int index;
+        Static switcher;
+        for (int i = 0; i < gar.Length - 1; i++)
+            {
+                bool flag = false;
+                max = gar[i].open_count;
+                index = i;
+
+                for (int j = i + 1; j < gar.Length; j++)
+                {
+                    if (gar[j].open_count > max)
+                    {
+                        max = gar[j].open_count;
+                        index = j;
+                        flag = true;
+                    }
+                }
+                if (flag == true)
+                {
+                    switcher = gar[index];
+                    gar[index] = gar[i];
+                    gar[i] = switcher;
+                    flag = false;
+                }
+            }
+        return gar;
+    } 
     public static void Main(string[] args)
     {
         Console.Write("Введите количество опрошенных: ");
@@ -221,20 +254,14 @@ public class Program
             double k = 0;
             for (int j = 0; j < count; j++)
             {
-                if (a == table[j].open_animal)
-                {
-                    k++;
-                }
+                if (a == table[j].open_animal) k++;
                 animal[i] = new Static(a, 100 * k / count);
             }
             p = table[i].open_personality;
             k = 0;
             for (int j = 0; j < count; j++)
             {
-                if (p == table[j].open_personality)
-                {
-                    k++;
-                }
+                if (p == table[j].open_personality) k++;
                 personality[i] = new Static(p, 100 * k / count);
             }
 
@@ -242,52 +269,17 @@ public class Program
             k = 0;
             for (int j = 0; j < count; j++)
             {
-                if (t == table[j].open_thing)
-                {
-                    k++;
-                }
+                if (t == table[j].open_thing) k++;
                 thing[i] = new Static(t, 100 * k / count);
             }
         }
-        double max;
-        int index;
-        Static switcher;
-        Static[] gar = animal;
-        for (int l = 0; l < 3; l++)
-        {
-            if (l == 0) gar = animal;
-            if (l == 1) gar = personality;
-            if (l == 2) gar = thing;
+        var sort = new Program();
+        animal = sort.Sort(animal);
+        personality = sort.Sort(personality);
+        thing = sort.Sort(thing);
 
-            for (int i = 0; i < gar.Length - 1; i++)
-            {
-                bool flag = false;
-                max = gar[i].open_count;
-                index = i;
-
-                for (int j = i + 1; j < gar.Length; j++)
-                {
-                    if (gar[j].open_count > max)
-                    {
-                        max = gar[j].open_count;
-                        index = j;
-                        flag = true;
-                    }
-                }
-                if (flag == true)
-                {
-                    switcher = gar[index];
-                    gar[index] = gar[i];
-                    gar[i] = switcher;
-                    flag = false;
-                }
-            }
-            if (l == 0) animal = gar;
-            if (l == 1) personality = gar;
-            if (l == 2) thing = gar;
-        }
         string[] gar2 = answer_animal;
-        gar = animal;
+        Static[] gar = animal;
         for (int l = 0; l < 3; l++)
         {
             Console.Write("\n\nВопрос №{0:d}\nНаиболее частный ответы:\tДолю встречи, %", l + 1);
@@ -302,10 +294,10 @@ public class Program
                 gar2 = answer_thing;
             }
             int k = 0;
-            for (int i=0; i<gar.Length;i++)
+            for (int i = 0; i < gar.Length; i++)
             {
                 if (k == 5) break;
-                if ((i >= 1) && (gar2[i - 1] != gar[i].open_answer)) 
+                if ((i >= 1) && (gar2[i - 1] != gar[i].open_answer))
                 {
                     gar[i].display();
                     k++;
